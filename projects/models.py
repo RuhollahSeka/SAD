b0301a1104a1763b2daea2492bc86b0893b6360f
msi1 <r.sekaleshfar@gmail.com>
2018-07-13 01:50:58 +0430

    Minor Clean Up

--- projects/models.py
+++ projects/models.py
@@ -12,10 +12,6 @@ from django.utils import timezone
 
 # Create your models here.
 
-
-
-
-
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
     charity = models.ForeignKey(Charity, on_delete=models.CASCADE, primary_key=False)
@@ -27,8 +23,8 @@ class Project(models.Model):
 class FinancialProject(Project):
     # TODO set min
 
-    target_money = models.FloatField()
-    current_money = models.FloatField()
+    target_money = models.FloatField
+    current_money = models.FloatField
 
 
 # TODO delete if useless
@@ -36,66 +32,57 @@ class NonFinancialProject(Project):
     pass
 
 
-
-
 class AbilityType(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
     pass
 
+
 class Ability(models.Model):
-    benefactor = models.ForeignKey(Benefactor,on_delete=models.CASCADE())
-    ability_type = models.ForeignKey(AbilityType,on_delete=models.CASCADE())
-    score = models.IntegerField()
-    description=models.CharField(max_length=300)
-
-
+    benefactor = models.ForeignKey(Benefactor, on_delete=models.CASCADE)
+    ability_type = models.ForeignKey(AbilityType, on_delete=models.CASCADE)
+    score = models.IntegerField
+    description = models.CharField(max_length=300)
 
 
 class Requirement(models.Model):
     project = models.ForeignKey(NonFinancialProject, on_delete=models.CASCADE, primary_key=False)
     ability_types = models.ManyToManyField(AbilityType, models.DO_NOTHING, primary_key=False)
-    min_age = models.IntegerField()
-    max_age = models.IntegerField()
+    min_age = models.IntegerField
+    max_age = models.IntegerField
     gender = models.CharField(max_length=100)
     location = models.CharField(max_length=1000)
-    require_quantity = models.IntegerField()
-
-
-
-
+    require_quantity = models.IntegerField
 
 
 class DateInterval(models.Model):
     user = models.OneToOneField(Project, on_delete=models.CASCADE, null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    # TODO can someone pls check what is going on with defining 2 user fields?
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     # project
     begin_date = models.DateField
     end_date = models.DateField
 
-    begin_saturday_1  = models.TimeField
-    end_saturday_1    = models.TimeField
+    begin_saturday_1 = models.TimeField
+    end_saturday_1 = models.TimeField
 
-    begin_sunday_1    = models.TimeField
-    end_sunday_1      = models.TimeField
+    begin_sunday_1 = models.TimeField
+    end_sunday_1 = models.TimeField
 
-    begin_monday_1    = models.TimeField
-    end_monday_1      = models.TimeField
+    begin_monday_1 = models.TimeField
+    end_monday_1 = models.TimeField
 
-    begin_tuesday_1   = models.TimeField
-    end_tuesday_1     = models.TimeField
+    begin_tuesday_1 = models.TimeField
+    end_tuesday_1 = models.TimeField
 
     begin_wednesday_1 = models.TimeField
-    end_wednesday_1   = models.TimeField
+    end_wednesday_1 = models.TimeField
 
-    begin_thursday_1  = models.TimeField
-    end_thursday_1    = models.TimeField
+    begin_thursday_1 = models.TimeField
+    end_thursday_1 = models.TimeField
 
-    begin_friday_1    = models.TimeField
-    end_friday_1      = models.TimeField
-
-
-
+    begin_friday_1 = models.TimeField
+    end_friday_1 = models.TimeField
 
     begin_saturday_2 = models.TimeField
     end_saturday_2 = models.TimeField
@@ -117,9 +104,6 @@ class DateInterval(models.Model):
 
     begin_friday_2 = models.TimeField
     end_friday_2 = models.TimeField
-
-
-
 
 
 class Request(models.Model):
