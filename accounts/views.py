39bb5903137188d765706cd57223e8aecde74a3e
A.Zed <zohri.alireza@gmail.com>
2018-07-27 13:01:05 +0430

    requests and notiffs?

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import TemplateView
+from django.template import loader
+
+from projects.models import NonFinancialProject, Project
+from accounts.models import *
+
 
 # Create your views here.
+
+def submit_benefactor_score(request):
+    try:
+        benefactor = Benefactor.objects.get(user=User.objects.get(username=request.POST.get('benefactor_username')))
+        ability = AbilityType.objects.get(benefactor=benefactor, name=request.POST.get('ability_type'))
+        score = BenefactorScore.objects.create(ability_type=ability, benefactor=benefactor,
+                                               charity=Charity.objects.get(user=request.user))
+        score.score = int(request.POST.get('score'))
+        score.save()
+        return HttpResponseRedirect([])
+    except:
+        return HttpResponse([])
+        # TODO raise error
+
+
+def submit_charity_score(request):
+    try:
+        charity = Charity.objects.get(user=User.objects.get(username=request.POST.get('charity_username')))
+        score = CharityScore.objects.get(charity=charity, benefactor=Benefactor.objects.get(user=request.user))
+        score.score = int(request.POST.get('score'))
+        score.save()
+        return HttpResponseRedirect([])
+    except:
+        # TODO raise error
+        return HttpResponse([])
+
+
+def submit_ability_request(request):
+    try:
+        new_request = AbilityRequest.objects.create(type=request.POST.get('type'), name=request.POST.get('name'),
+                                                    description=request.POST.get('description'))
+        new_request.save()
+        return HttpResponseRedirect([])
+    except:
+        # TODO Raise Error
+        return HttpResponse([])
+
+
+def submit_cooperation_request(request):
+    try:
+        if request.user.is_benefactor:
+            benefactor = Benefactor.objects.get(user=request.user)
+            charity = Charity.objects.get(user=User.objects.get(username=request.POST.get('username')))
+            request_type = 'b2c'
+        else:
+            benefactor = Benefactor.objects.get(user=User.objects.get(username=request.POST.get('username')))
+            charity = Charity.objects.get(user=request.user)
+            request_type = 'c2b'
+        # FIXME How should we find the project? I mean which data is given to find the project with?
+        project = NonFinancialProject.objects.get()
+        new_request = CooperationRequest.objects.create(benefactor=benefactor, charity=charity, type=request_type,
+                                                        description=request.POST.get('description'))
+        return HttpResponseRedirect([])
+    except:
+        # TODO Raise Error
+        return HttpResponse('error')
