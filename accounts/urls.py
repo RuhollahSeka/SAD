dd25621f7a354d82d2e5bb2635026b4b3ec8b2b3
msi1 <r.sekaleshfar@gmail.com>
2018-07-30 22:27:04 +0430

    fix customize user view

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('accounts/login/', views.LoginView.as_view(), name='login_view'),
     path('accounts/login/register/', views.login_user, name='login_user'),
 
-    path('accounts/customize_user/', views.CustomizeUserView.as_view(), name='customize_user_view'),
+    path('accounts/customize_user/', views.customize_user_data, name='customize_user_view'),
     path('accounts/customize_user/register/', views.customize_user, name='customize_user'),
 ]
 
