8db724c8743d2d9932f84b8180bda9230d6e37ed
A.Zed <zohri.alireza@gmail.com>
2018-08-01 11:40:04 +0430

    added comments and fixed .get methods

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,11 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import TemplateView
-from django.template import loader
 
 from projects.models import FinancialProject, NonFinancialProject, Project, Log, Ability
-from accounts.models import *
-
 ####### Danial imports .Some of them may be redundant!!!
 
 from django.contrib.auth import login
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect
 from django.views.generic import CreateView, TemplateView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -20,6 +14,17 @@ from django.template import loader
 from accounts.forms import CharitySignUpForm
 from accounts.models import *
 from projects.models import CooperationRequest
+
+
+def get_object(obj_class, *args, **kwargs):
+    try:
+        obj_set = obj_class.objects.filter(*args, **kwargs)
+        if obj_set.count() <= 0:
+            return None
+        return obj_set.all()[0]
+    except:
+        # TODO Raise Super Ultra Error
+        return 'Error'
 
 
 # Create your views here.
@@ -99,7 +104,7 @@ def admin_first_page_data(request):
     })
 
 
-def submit_benefactor_score(request):
+def submit_benefactor_score(request, benefactor_username):
     if not request.user.is_authenticated:
         # TODO Raise Authentication Error
         context = {
@@ -112,20 +117,20 @@ def submit_benefactor_score(request):
             'error_message': 'Account Type Error: You Can\'t Submit Score for Another Benefactor!'
         }
         return HttpResponse([])
-    charity = Charity.objects.get(user=request.user)
-    benefactor = Benefactor.objects.get(user=User.objects.get(username=request.POST.get('benefactor_username')))
-    if charity.benefactor_history.filter(user=benefactor.user).count() <= 0:
-        context = {
-            'error_message': ' No Cooperation Error: You Cannot Submit Score for a Benefactor with Whom You Had no Cooperation!'
-        }
-        # TODO Raise No_Cooperation Error
-        return HttpResponse([])
     try:
-        ability = AbilityType.objects.get(benefactor=benefactor, name=request.POST.get('ability_type'))
+        charity = get_object(Charity, user=request.user)
+        benefactor = get_object(Benefactor, user=get_object(User, username=benefactor_username))
+        if charity.benefactor_history.filter(user=benefactor.user).count() <= 0:
+            context = {
+                'error_message': 'No Cooperation Error: You Cannot Submit Score for a Benefactor with Whom You Had no Cooperation!'
+            }
+            # TODO Raise No_Cooperation Error
+            return HttpResponse([])
+        ability = get_object(AbilityType, benefactor=benefactor, name=request.POST.get('ability_type'))
         score = charity.benefactorscore_set.get(benefactor=benefactor, charity=charity)
         if score is None:
             score = BenefactorScore.objects.create(ability_type=ability, benefactor=benefactor,
-                                                   charity=Charity.objects.get(user=request.user))
+                                                   charity=get_object(Charity, user=request.user))
         score.score = int(request.POST.get('score'))
         score.save()
         return HttpResponseRedirect([])
@@ -150,18 +155,18 @@ def submit_charity_score(request):
         }
         # TODO Raise Account Type Error
         return HttpResponse([])
-    benefactor = Benefactor.objects.get(user=request.user)
-    if benefactor.charity_set.get(user=User.objects.get(username=request.POST.get('charity_username'))).count <= 0:
+    benefactor = get_object(Benefactor, user=request.user)
+    if benefactor.charity_set.get(user=get_object(User, username=request.POST.get('charity_username'))).count <= 0:
         context = {
             'error_message': ' No Cooperation Error: You Cannot Submit Score for a Charity with Which You Had no Cooperation!'
         }
         # TODO Raise No_Cooperation Error
         return HttpResponse([])
     try:
-        charity = Charity.objects.get(user=User.objects.get(username=request.POST.get('charity_username')))
+        charity = get_object(Charity, user=get_object(User, username=request.POST.get('charity_username')))
         score = benefactor.charityscore_set.get(benefactor=benefactor, charity=charity)
         if score is None:
-            score = CharityScore.objects.get(charity=charity, benefactor=Benefactor.objects.get(user=request.user))
+            score = get_object(CharityScore, charity=charity, benefactor=get_object(Benefactor, user=request.user))
         score.score = int(request.POST.get('score'))
         score.save()
         return HttpResponseRedirect([])
@@ -204,16 +209,18 @@ def submit_cooperation_request(request, project_id):
         # FIXME How should we find the project? I mean which data is given to find the project with?
         project = NonFinancialProject.objects.all().filter(project_id=project_id)
         if request.user.is_benefactor:
-            benefactor = Benefactor.objects.get(user=request.user)
-            charity = Charity.objects.get(user=User.objects.get(username=request.POST.get('username')))
-            new_notification = Notification.objects.create(type='new_request',user=charity.user, datetime=datetime.datetime.now())
+            benefactor = get_object(Benefactor, user=request.user)
+            charity = get_object(Charity, user=get_object(User, username=request.POST.get('username')))
+            new_notification = Notification.objects.create(type='new_request', user=charity.user,
+                                                           datetime=datetime.datetime.now())
             new_notification.description = 'A new Cooperation Request is Received for Project ' + project.project
             new_notification.save()
             request_type = 'b2c'
         else:
-            benefactor = Benefactor.objects.get(user=User.objects.get(username=request.POST.get('username')))
-            charity = Charity.objects.get(user=request.user)
-            new_notification = Notification.objects.create(type='new_request',user=benefactor.user, datetime=datetime.datetime.now())
+            benefactor = get_object(Benefactor, user=get_object(User, username=request.POST.get('username')))
+            charity = get_object(Charity, user=request.user)
+            new_notification = Notification.objects.create(type='new_request', user=benefactor.user,
+                                                           datetime=datetime.datetime.now())
             new_notification.description = 'A new Cooperation Request Has Been Received!'
             new_notification.save()
             request_type = 'c2b'
@@ -274,17 +281,15 @@ def signup(request):
         test1_user = User.objects.filter(username=request.POST.get("username"))
         test2_user = User.objects.filter(username=request.POST.get("email"))
         if test1_user.__len__() != 0 and test2_user.__len__() != 0:
-
-            return render(request, 'accounts/register.html', {'error_message': 'Account already exists! Try login or forget password.'})
+            return render(request, 'accounts/register.html',
+                          {'error_message': 'Account already exists! Try login or forget password.'})
 
         if test1_user.__len__() == 0 and len(test2_user) != 0:
             return render(request, 'accounts/register.html', {'error_message': 'Email is already taken!  '})
 
         if len(test1_user) != 0 and len(test2_user) == 0:
-            return render(request, 'accounts/register.html', {'error_message': 'Username is already taken! try another username.  '})
-
-
-
+            return render(request, 'accounts/register.html',
+                          {'error_message': 'Username is already taken! try another username.  '})
 
         tmp_contact_info = ContactInfo.objects.create(country="Iran",
                                                       province=request.POST.get("province"),
@@ -341,7 +346,7 @@ def login_user(request):
         tmp_user = User.objects.filter(username=request.POST.get("username"))
         if len(tmp_user) == 0:
             return render(request, 'accounts/login.html')
-        tmp_user = User.objects.get(username=request.POST.get("username"))
+        tmp_user = get_object(User, username=request.POST.get("username"))
         if tmp_user.password != request.POST.get("password"):
             return render(request, 'accounts/login.html', {'error_msg': 'Invalid password -.-'})
 
@@ -377,34 +382,34 @@ def user_profile(request):
                    "phone_number": request.user.contact_info.phone_number}
         if request.user.is_benefactor:
             # try:
-                benefactor = Benefactor.objects.get(user=request.user)
-                context['user'] = benefactor
-                context['benefactor'] = benefactor
-                projects = {project for project in Project.objects.all() if benefactor in project.benefactors}
-                context['project_count'] = len(projects)
-                abilities = benefactor.ability_set.all()
-                if benefactor.score == -1:
-                    score = 'N/A'
-                else:
-                    score = 0
-                    for ability in abilities:
-                        score += ability.score
-                    score /= abilities
-                print(request)
-                context['score'] = score
-                context["first_name"] = benefactor.first_name
-                context["last_name"] = benefactor.last_name
-                context["gender"] = benefactor.gender
-                context["age"] = benefactor.age
-                context["credit"] = benefactor.credit
-                print(context)
-                # print(context.__str__())
+            benefactor = get_object(Benefactor, user=request.user)
+            context['user'] = benefactor
+            context['benefactor'] = benefactor
+            projects = {project for project in Project.objects.all() if benefactor in project.benefactors}
+            context['project_count'] = len(projects)
+            abilities = benefactor.ability_set.all()
+            if benefactor.score == -1:
+                score = '-'
+            else:
+                score = 0
+                for ability in abilities:
+                    score += ability.score
+                score /= abilities
+            print(request)
+            context['score'] = score
+            context["first_name"] = benefactor.first_name
+            context["last_name"] = benefactor.last_name
+            context["gender"] = benefactor.gender
+            context["age"] = benefactor.age
+            context["credit"] = benefactor.credit
+            print(context)
+            # print(context.__str__())
             # except:
             #     print(1)
 
         else:
             try:
-                charity = Charity.objects.get(user=request.user)
+                charity = get_object(Charity, user=request.user)
                 context["name"] = charity.name
                 context["score"] = charity.score
             except:
@@ -431,11 +436,11 @@ def customize_user_data(request):
         context = {"type": request.user.is_charity, "username": request.user.username, "email": request.user.email,
                    "country": request.user.contact_info.country, "province": request.user.contact_info.province,
                    "city": request.user.contact_info.city, "address": request.user.contact_info.address,
-                   "phone_number": request.user.contact_info.phone_number,"description": request.user.description,
+                   "phone_number": request.user.contact_info.phone_number, "description": request.user.description,
                    "notifications": notifications}
         if request.user.is_benefactor:
             try:
-                benefactor = Benefactor.objects.get(user=request.user)
+                benefactor = get_object(Benefactor, user=request.user)
                 projects = {project for project in Project.objects.all() if benefactor in project.benefactors}
                 context['project_count'] = len(projects)
                 abilities = benefactor.ability_set.all()
@@ -519,12 +524,12 @@ def add_benefactor_credit(request):
         }
         return HttpResponse([])
     # try:
-    benefactor = Benefactor.objects.get(user=request.user)
+    benefactor = get_object(Benefactor, user=request.user)
     amount = int(request.POST.get('deposit_amount'))
     benefactor.credit += amount
     benefactor.save()
     # FIXME Redirect to user profile view
-    return render(request, 'accounts/user-profile.html')
+    return HttpResponseRedirect(reverse('user_profile'))
     # except:
     #     context = {
     #         'error_message': 'error in deposit!',
@@ -533,3 +538,83 @@ def add_benefactor_credit(request):
     #     }
     #     return HttpResponseRedirect(reverse('error'))
 
+
+def submit_benefactor_commit(request, benefactor_username):
+    if not request.user.is_authenticated:
+        # TODO Raise Authentication Error
+        context = {
+            'error_message': 'Authentication Error: You are not Signed In!'
+        }
+        return HttpResponse([])
+    if request.user.is_benefactor:
+        # TODO Raise Account Type Error
+        context = {
+            'error_message': 'Account Type Error: Benefactors Cannot Comment on Other Benefactors',
+        }
+        return HttpResponse([])
+    benefactor_users = User.objects.filter(username=benefactor_username)
+    if benefactor_users.count() <= 0:
+        # TODO Raise Not Found Error
+        context = {
+            'error_message': '404 Not Found: Requested User Cannot be Found'
+        }
+        return HttpResponse([])
+    benefactor_user = benefactor_users.all()[0]
+    try:
+        benefactor = get_object(Benefactor, user=get_object(User, username=benefactor_username))
+        if request.user.charity.benefactor_history.filter(user=benefactor.user).count() <= 0:
+            context = {
+                'error_message': 'No Cooperation Error: You Cannot Submit Score for a Benefactor with Whom You Had no Cooperation!'
+            }
+            # TODO Raise No_Cooperation Error
+            return HttpResponse([])
+        comment = BenefactorComment.objects.create(commented=benefactor_user.benefactor, commentor=request.user.charity,
+                                                   comment_string=request.POST.get('comment_string'))
+        # TODO Redirect to Benefactor Profile Page
+        return HttpResponseRedirect([])
+    except:
+        # TODO Raise Unexpcted Error
+        context = {
+            'error_message': 'Unexpected Error: Error While Submitting The comment'
+        }
+        return HttpResponse([])
+
+
+def submit_charity_commit(request, charity_username):
+    if not request.user.is_authenticated:
+        # TODO Raise Authentication Error
+        context = {
+            'error_message': 'Authentication Error: You are not Signed In!'
+        }
+        return HttpResponse([])
+    if request.user.is_charity:
+        # TODO Raise Account Type Error
+        context = {
+            'error_message': 'Account Type Error: Charities Cannot Comment on Other Charities',
+        }
+        return HttpResponse([])
+    if request.user.benefactor.charity_set.get(user=get_object(User, username=charity_username)).count <= 0:
+        context = {
+            'error_message': ' No Cooperation Error: You Cannot Submit Score for a Charity with Which You Had no Cooperation!'
+        }
+        # TODO Raise No_Cooperation Error
+        return HttpResponse([])
+    charity_users = User.objects.filter(username=charity_username)
+    if charity_users.count() <= 0:
+        # TODO Raise Not Found Error
+        context = {
+            'error_message': '404 Not Found: Requested User Cannot be Found'
+        }
+        return HttpResponse([])
+    charity_user = charity_users.all()[0]
+    try:
+        comment = CharityComment.objects.create(commented=charity_user.charity, commentor=request.user.benefactor,
+                                                comment_string=request.POST.get('comment_string'))
+        # TODO Redirect to Charity Profile Page
+        return HttpResponseRedirect([])
+    except:
+        # TODO Raise Unexpected Error
+        context = {
+            'error_message': 'Unexpected Error: Error While Submitting The comment'
+        }
+        return HttpResponse([])
